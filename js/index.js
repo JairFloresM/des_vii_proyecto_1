6d21447ac28bad4dca3acc4b1a467a9e5ccba768
Jair♥ <jairfm01@gmail.com>
2022-10-09 02:09:30 -0500

    anexo del push pasado

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const actividades = document.querySelectorAll('.fa-chevron-up');
+
+
+actividades.forEach(acti => {
+    acti.addEventListener('click', (e) => {
+        acti.parentNode.parentNode.parentNode.children[1].classList.toggle('card_ocultar')
+        acti.classList.toggle('arrow-move');
+    })
+})
+
