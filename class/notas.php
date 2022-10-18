0185994b4d26bf5725eb6561b9b714e23e8e1401
Ranses Vergara <106293764+Ryuurv04@users.noreply.github.com>
2022-10-18 10:47:11 -0500

    Update notas.php

--- class/notas.php
+++ class/notas.php
@@ -85,6 +85,35 @@ class Nota extends Conexion
 
         return $stmt;
     }
+    public function filtrar_nota($filtro, $dato)
+    {
+        $notas = [];
+        $query = "SELECT n.id, n.titulo, n.fecha, n.ubicacion, n.correo, n.repetir, n.tiempo_repetir_hora, a.descripcion FROM notas n
+                  INNER JOIN actividades a ON n.id_actividad = a.id";
+
+        if ($filtro == "descripcion")
+            $query .= " WHERE descripcion = ?";
+        else if ($filtro == "day")
+            $query .= " WHERE CAST(day(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "month")
+            $query .= " WHERE CAST(month(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "year")
+            $query .= " WHERE CAST(year(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "week")
+            $query .= " WHERE CAST(week(n.fecha) AS CHAR) = ?";
+
+        $stmt = $this->_db->prepare($query);
+        $stmt->bind_param('s', $dato);
+        $stmt->execute();
+        $resp = $stmt->get_result();
+
+
+        while ($nota = $resp->fetch_array(MYSQLI_ASSOC)) {
+            array_push($notas, $nota);
+        }
+
+        return $notas;
+    }
     
     public function notas_hoy()
     {
