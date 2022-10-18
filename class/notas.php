eb4f47b15a2d1642d11aefd34dca17aed17726ab
Jair♥ <jairfm01@gmail.com>
2022-10-18 06:49:42 -0500

    se arreglo la parte de agregar y tambien se creo el reporte general

--- class/notas.php
+++ class/notas.php
@@ -31,7 +31,6 @@ class Nota extends Conexion
 
         while ($nota = $resp->fetch_array(MYSQLI_ASSOC)) {
             array_push($notas, $nota);
-            //print_r($nota);
         }
         return $notas;
     }
@@ -44,5 +43,65 @@ class Nota extends Conexion
         $stmt->execute();
 
         return $stmt;
+    }
+
+    public function agregar_nota($titulo, $fecha, $hora, $ubicacion, $correo, $repetir, $tiempo_repetir, $actividad)
+    {
+        $query = "CALL sp_crear_nota(?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->_db->prepare($query);
+        $stmt->bind_param('sssssisi', $titulo, $fecha, $hora, $ubicacion, $correo, $repetir, $tiempo_repetir, $actividad);
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+
+
+    //No me funciono el filtrar, asique me invente una cochinada
+    public function filtrar_nota($filtro, $dato)
+    {
+        $notas = [];
+        $query = "SELECT n.id, n.titulo, n.fecha, n.ubicacion, n.correo, n.repetir, n.tiempo_repetir_hora, a.descripcion FROM notas n
+                  INNER JOIN actividades a ON n.id_actividad = a.id";
+
+        if ($filtro == "descripcion")
+            $query .= " WHERE descripcion = ?";
+        else if ($filtro == "day")
+            $query .= " WHERE CAST(day(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "month")
+            $query .= " WHERE CAST(month(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "year")
+            $query .= " WHERE CAST(year(n.fecha) AS CHAR) = ?";
+        else if ($filtro == "week")
+            $query .= " WHERE CAST(week(n.fecha) AS CHAR) = ?";
+
+        $stmt = $this->_db->prepare($query);
+        $stmt->bind_param('s', $dato);
+        $stmt->execute();
+        $resp = $stmt->get_result();
+
+
+        while ($nota = $resp->fetch_array(MYSQLI_ASSOC)) {
+            array_push($notas, $nota);
+        }
+
+        return $notas;
+    }
+
+
+    public function notas_hoy()
+    {
+        $notas = [];
+        $query = "CALL sp_notas_hoy()";
+
+        $stmt = $this->_db->prepare($query);
+        $stmt->execute();
+        $resp = $stmt->get_result();
+
+        while ($nota = $resp->fetch_array(MYSQLI_ASSOC)) {
+            array_push($notas, $nota);
+        }
+
+        return $notas;
     }
 }
